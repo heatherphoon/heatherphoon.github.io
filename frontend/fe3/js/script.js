548edfe44f2afa620b5e3a56d4cd9fef968b8c83
heatherphoon <phoon_yong_shian@hotmail.com>
2016-08-17 12:49:59 +0800

    Update script.js

--- frontend/fe3/js/script.js
+++ frontend/fe3/js/script.js
@@ -1,1 +1,12 @@
-new js file
+function startTime() {
+  var today = new Date();
+  var hr = today.getHours();
+var min = today.getMinutes();
+var sec = today.getSeconds();
+ap = (hr < 12 ) ? "<span>AM</span>" : "<span>PM</span>" ;
+hr = (hr == 0) ? 12 : hr;
+hr = (hr > 12) ? hr - 12 : hr ;
+hr = checkTime(hr);
+min = checkTime(min);
+sec = checkTime(sec);
+}
