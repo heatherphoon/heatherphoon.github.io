8f186d2f339e7faf13243d5b156c751f885ae9b2
heatherphoon <phoon_yong_shian@hotmail.com>
2016-10-29 21:10:39 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -84,3 +84,7 @@ $(document).foundation();
     
       var doc = document.documentElement;
       doc.setAttribute('data-useragent', navigator.userAgent);
+
+$(document).ready(function(){
+    $("body").hide(0).delay(500).fadeIn(3000)
+});
