44f58436eae82546015ac189520af412f9dbebc7
heatherphoon <phoon_yong_shian@hotmail.com>
2016-11-05 21:12:29 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -60,6 +60,7 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+//parallax
 $(function(){
 
   var parallax = document.querySelectorAll(".parallax"),
@@ -82,9 +83,13 @@ $("#fade").css("opacity", 1 - $(window).scrollTop() / 350);
 $(document).foundation();  
       $(document).animateScroll();
     
-      var doc = document.documentElement;
-      doc.setAttribute('data-useragent', navigator.userAgent);
 
+//delay onload
 $(document).ready(function(){
-    $("body").hide(0).delay(500).fadeIn(3000)
+    $("nav").hide(0).delay(500).fadeIn(3000)
 });
+
+function loading(){
+$("#loader").delay(300).fadeOut(2000);
+    $("#loader div").delay(500).hide(0);
+}
